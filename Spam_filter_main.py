01e7901a43414717dfc1d14e90f269983fc7bc7d
AlexandreBoult <alex.boult@orange.fr>
2024-01-16 22:44:58 +0100

    Cross validation

--- Spam_filter_main.py
+++ Spam_filter_main.py
@@ -7,11 +7,12 @@ from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV,ShuffleSplit
 
 
 df=pd.read_table("SMSSpamCollection")
 
-
+#old code
 """
 vectorizer = CountVectorizer() 
 wm = vectorizer.fit_transform(df['msg'].tolist())
@@ -30,20 +31,23 @@ model = MLPClassifier(random_state=1, max_iter=200)
 model.fit(X_train, y_train)
 """
 
-
-
-
-
 vectorize_pl = make_pipeline(CountVectorizer(),FunctionTransformer(lambda x: x.toarray(), accept_sparse=True))
 
-pl=make_pipeline(vectorize_pl,PCA(n_components=200),MLPClassifier(random_state=1, max_iter=200))
+pl=make_pipeline(vectorize_pl,PCA(n_components=200),MLPClassifier(random_state=1,max_iter=600,early_stopping=False,learning_rate='adaptive',solver='adam',warm_start=False))
 
 X = df['msg']
 y = df['cat']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-pl.fit(X_train, y_train)
-y_pred = pl.predict(X_test)
-
-score = accuracy_score(y_test, y_pred)
-print("score :", round(score, 5))
+opti=0
+if opti: #hyperparameters optimsation
+    shuffle_split = ShuffleSplit(n_splits=5,test_size=0.3)
+    X_part=X.sample(frac=0.5)
+    param_grid = {"mlpclassifier__solver":['lbfgs','sgd','adam'],"mlpclassifier__warm_start":[True,False],"mlpclassifier__learning_rate":['constant','invscaling','adaptive'],"mlpclassifier__early_stopping":[True,False]}
+    search=GridSearchCV(pl,param_grid,verbose=10,cv=shuffle_split).fit(X_part,y[X_part.index])
+    print(search.best_params_)
+else:
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    pl.fit(X_train, y_train)
+    y_pred = pl.predict(X_test)
+    score = accuracy_score(y_test, y_pred)
+    print("score :", round(score, 5))
