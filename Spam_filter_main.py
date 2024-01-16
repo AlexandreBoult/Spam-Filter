39267408b4714e4078b8f199d7f8db2d166c81d0
AlexandreBoult <alex.boult@orange.fr>
2024-01-16 19:43:40 +0100

    pipeline

--- Spam_filter_main.py
+++ Spam_filter_main.py
@@ -1,44 +1,50 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import LabelEncoder
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import make_pipeline
 from sklearn.neural_network import MLPClassifier
-df=pd.read_table("SMSSpamCollection")
-print(df)
+from sklearn.decomposition import PCA
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.model_selection import train_test_split
 
+
+
+df=pd.read_table("SMSSpamCollection")
+
+
+"""
 vectorizer = CountVectorizer() 
 wm = vectorizer.fit_transform(df['msg'].tolist())
 tokens=vectorizer.get_feature_names_out()
 df_vect=pd.DataFrame(data=wm.toarray(),columns=tokens)
 
-print(df_vect)
-from sklearn.decomposition import PCA
 pca=PCA(n_components=200)
+
 reduced_df=pd.DataFrame(pca.fit_transform(df_vect))
-print(reduced_df)
 
-from sklearn.model_selection import train_test_split
-X = reduced_df
-"""
-transfo_target = Pipeline(steps=[
-    ('Label_enco', LabelEncoder())
-])
-"""
-
+X = df['msg']
 y = df['cat']
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-"""
-model = Pipeline(steps=[
-                        ('model',KNeighborsClassifier(3,p=1))])
-"""
-#model = GaussianNB()
+
 model = MLPClassifier(random_state=1, max_iter=200)
 model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+"""
+
+
+
+
+
+vectorize_pl = make_pipeline(CountVectorizer(),FunctionTransformer(lambda x: x.toarray(), accept_sparse=True))
+
+pl=make_pipeline(vectorize_pl,PCA(n_components=200),MLPClassifier(random_state=1, max_iter=200))
+
+X = df['msg']
+y = df['cat']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+pl.fit(X_train, y_train)
+y_pred = pl.predict(X_test)
 
 from sklearn.metrics import accuracy_score
-knn_score = accuracy_score(y_test, y_pred)
-print("score :", round(knn_score, 5))
+score = accuracy_score(y_test, y_pred)
+print("score :", round(score, 5))
