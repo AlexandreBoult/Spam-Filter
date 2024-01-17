6fbb16e1d29f4586f3cefe0a482b772b10d608a8
AlexandreBoult <alex.boult@orange.fr>
2024-01-17 14:14:15 +0100

    slightly better precision

--- Spam_filter_main.py
+++ Spam_filter_main.py
@@ -7,7 +7,7 @@ from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV,ShuffleSplit
+from sklearn.model_selection import GridSearchCV,ShuffleSplit,RandomizedSearchCV
 
 
 df=pd.read_table("SMSSpamCollection")
@@ -33,7 +33,7 @@ model.fit(X_train, y_train)
 
 vectorize_pl = make_pipeline(CountVectorizer(),FunctionTransformer(lambda x: x.toarray(), accept_sparse=True))
 
-pl=make_pipeline(vectorize_pl,PCA(n_components=200),MLPClassifier(random_state=1,max_iter=600,early_stopping=False,learning_rate='constant',solver='adam',warm_start=False))
+pl=make_pipeline(vectorize_pl,PCA(n_components=200),MLPClassifier(random_state=1,solver='sgd',max_iter=400,hidden_layer_sizes=(120, 80, 40),alpha=0.05))
 
 X = df['msg']
 y = df['cat']
@@ -42,8 +42,15 @@ opti=0
 if opti: #hyperparameters optimsation
     shuffle_split = ShuffleSplit(n_splits=5,test_size=0.3)
     X_part=X.sample(frac=0.5)
-    param_grid = {"mlpclassifier__solver":['lbfgs','sgd','adam'],"mlpclassifier__warm_start":[True,False],"mlpclassifier__learning_rate":['constant','invscaling','adaptive'],"mlpclassifier__early_stopping":[True,False]}
-    search=GridSearchCV(pl,param_grid,verbose=10,cv=shuffle_split).fit(X_part,y[X_part.index])
+    param_grid = {
+    'mlpclassifier__hidden_layer_sizes': [(150,100,50), (120,80,40), (100,50,30), (100,)],
+    'mlpclassifier__max_iter': [50, 100, 150, 200, 300, 400],
+    'mlpclassifier__activation': ['tanh', 'relu'],
+    'mlpclassifier__solver': ['sgd', 'adam'],
+    'mlpclassifier__alpha': [0.0001, 0.05],
+    'mlpclassifier__learning_rate': ['constant','adaptive'],
+    }
+    search=RandomizedSearchCV(pl,param_grid,verbose=10,cv=shuffle_split,n_iter=30,random_state=1).fit(X_part,y[X_part.index])
     print(search.best_params_)
 else:
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=5)
